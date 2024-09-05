801fa63537d6572b034565a9653a9c3875cd94c5
Priyanshu Dubey <129979141+Priyanshu1119@users.noreply.github.com>
2024-09-05 17:17:02 +0000

    01_variable

---
+++ 01_variable.js
@@ -0,0 +1,20 @@
+const accountId=1234
+let accountEmail = "priyanshudubey2026@gmail.com"
+var accountpassword = "12345667"
+   accountCity = "Jaipur"
+   accountEmail = "priyanshudubey2024@gmail.com"
+   
+   accountpassword = "01293874"
+       accountCity = "Mumbai"
+      
+
+
+// accountId = 2
+console.log(accountEmail);
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+
+console.table([accountId,accountEmail,accountpassword,accountCity]);
